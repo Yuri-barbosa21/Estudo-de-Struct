7744a73b9e589ff39b9815f71b26b286348a46b6
Yuri <62626768+Yuri-barbosa21@users.noreply.github.com>
2022-04-21 21:32:59 +0000

    Como criar uma struct com dados lidos do teclado

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "string.h"
 
 //----------------- NOVO TIPO DE DADO COM TYPEDEF STRUCT ----------------
+/*
 typedef struct {
     int idade;
     char sexo;
@@ -14,12 +15,24 @@ struct Pessoa2 {
     char sexo;
     char nome[100];
 };
+*/
+//_______________________________________________________________________
+
+
+
+//------------- COMO CRIAR UMA STRUCT COM DADOS LIDOS DO TECLADO --------------
+typedef struct {
+    int idade;
+    char sexo;
+    char nome[100];
+}Pessoa;
 //_______________________________________________________________________
 
 
 int main(void) {
     
   //----------------- NOVO TIPO DE DADO COM TYPEDEF STRUCT ----------------
+    /*
     Pessoa pessoa1;
     struct Pessoa2 pessoa2;
 
@@ -28,6 +41,25 @@ int main(void) {
     strcpy(pessoa2.nome, "Maria Madalena");
 
     printf("Nome: %s\nIdade: %d\nSexo: %c\n", pessoa2.nome, pessoa2.idade, pessoa2.sexo);
+    */
+//_________________________________________________________________________
+
+    
+
+    
+//------------- COMO CRIAR UMA STRUCT COM DADOS LIDOS DO TECLADO --------------
+    
+    Pessoa pessoa;
+
+    printf("Digite seu nome: ");
+    fgets(pessoa.nome, 100, stdin);
+    printf("Digite sua idade: ");
+    scanf("%d", &pessoa.idade);
+    printf("Digie f ou m para o sexo: ");
+    scanf(" %c", &pessoa.sexo);
+
+    printf("\nNome: %sIdade: %d\nSexo: %c\n", pessoa.nome, pessoa.idade, pessoa.sexo);
+    
 //_________________________________________________________________________
 
 
