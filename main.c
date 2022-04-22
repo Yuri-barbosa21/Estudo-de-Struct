1e3444602d9cb409a7cd00770dcef519e1fc82cc
Yuri <62626768+Yuri-barbosa21@users.noreply.github.com>
2022-04-22 00:06:47 +0000

    Vetor de Struct

--- main.c
+++ main.c
@@ -77,6 +77,7 @@ void ImprimirPessoa(Pessoa p){
 
 
 //------------------------- FUNÇÃO QUE RETORNA UMA STRUCT -----------------------
+/*
 typedef struct {
     int dia, mes, ano;
 }DataNas;
@@ -109,9 +110,48 @@ Pessoa LerPessoa(){
     scanf("%d%d%d", &pessoa.DataNas.dia, &pessoa.DataNas.mes, &pessoa.DataNas.ano);
     return pessoa;
 }
-
+*/
 //______________________________________________________________________________
 
+
+
+// ------------------------------- VETOR DE STRUCT ------------------------------
+typedef struct {
+    int dia, mes, ano;
+}DataNas;
+
+typedef struct {
+    DataNas DataNas;
+    int idade;
+    char sexo;
+    char nome[100];
+}Pessoa;
+
+void ImprimirPessoa(Pessoa p){
+    
+    printf("\n\tNome: %s", p.nome);
+    printf("\tIdade: %d\n", p.idade);
+    printf("\tSexo: %c\n", p.sexo);
+    printf("\tData de Nascimento: %d/%d/%d\n", p.DataNas.dia, p.DataNas.mes, p.DataNas.ano);
+}
+
+Pessoa LerPessoa(){
+    Pessoa pessoa;
+
+    printf("\nDigite seu nome: ");
+    fgets( pessoa.nome, 100, stdin);
+    printf("Digite sua idade: ");
+    scanf("%d", &pessoa.idade);
+    printf("Digie f ou m para o sexo: ");
+    scanf(" %c", &pessoa.sexo);
+    printf("Digite sua data de nascimento no formato dd mm aaaa: ");
+    scanf("%d%d%d", &pessoa.DataNas.dia, &pessoa.DataNas.mes, &pessoa.DataNas.ano);
+    setbuf(stdin, NULL); //Limpar buffer do teclado
+    
+    return pessoa;
+}
+
+//_______________________________________________________________________________
 
 int main(void) {
     
@@ -192,13 +232,28 @@ int main(void) {
 
 
 //------------------------- FUNÇÃO QUE RETORNA UMA STRUCT -----------------------
-    
+    /*
     Pessoa pessoa;
 
     pessoa = LerPessoa();
     ImprimirPessoa(pessoa);
-    
+    */
 //______________________________________________________________________________ 
 
+
+
+// ------------------------------- VETOR DE STRUCT ------------------------------
+    int i;
+    Pessoa pessoas[3];
+
+    for(i = 0; i < 3; i++){
+        pessoas[i] = LerPessoa();
+    }
+    for(i = 0; i < 3; i++){
+        ImprimirPessoa(pessoas[i]);
+    }
+
+//_______________________________________________________________________________
+    
   return 0;
 }
